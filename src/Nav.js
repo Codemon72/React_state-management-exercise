eb2d52f681919aba9091753c048fec69e7426d07
Clemens Bruesch <clemensbruesch@gmail.com>
2021-04-24 21:05:57 +0200

    Nav component reads from useContext

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import {MovieContext} from './MovieContext';
 
 const Nav = () => {
+  const [movies, setMovies] = useContext(MovieContext);
+
   return (
     <nav>
       <h2>Codemon72</h2>
-      <p>List of Movies: </p>
+      <p>List of Movies: {movies.length}</p>
     </nav>
   )
 }
